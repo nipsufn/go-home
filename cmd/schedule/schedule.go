e3b3e2f32059e05da4ce48118583a3e2991e3b94
nipsufn <nipsufn+gh@pm.me>
2024-09-16 20:24:14 +0200

    feat: job persistance

--- cmd/schedule/schedule.go
+++ cmd/schedule/schedule.go
@@ -3,6 +3,10 @@ package schedule
 import (
 	"fmt"
 	"go-home/config"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
 	"time"
 
 	myJobs "go-home/cmd/schedule/jobs"
@@ -11,18 +15,38 @@ import (
 	log "github.com/sirupsen/logrus"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 type Job struct {
-	gorm.Model
-	Name     string
-	Schedule string
-	OpName   string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+	Name      string         `gorm:"primaryKey"`
+	Schedule  string
+	OpName    string
 }
 
 var (
-	db        *gorm.DB
-	scheduler gocron.Scheduler
+	db               *gorm.DB
+	scheduler        gocron.Scheduler
+	signals          chan os.Signal
+	onConflictClause = clause.OnConflict{
+		Columns:   []clause.Column{{Name: "name"}},
+		DoUpdates: clause.AssignmentColumns([]string{"schedule", "op_name"}),
+	}
+	defaultJobs = []Job{
+		{
+			Name:     "builtin_sunset_gen",
+			Schedule: "00 00 15 * * *",
+			OpName:   "sunset",
+		},
+		{
+			Name:     "userdef_wakeup-alarm",
+			Schedule: "00 20 07 * * *",
+			OpName:   "wakeup",
+		},
+	}
 )
 
 func bootstrapJob(opName string) error {
@@ -46,6 +70,36 @@ func doesJobWithNameExist(name string) bool {
 	return false
 }
 
+func interruptHandler() {
+	signal := <-signals
+	log.Infof("caught signal %v", signal)
+	var jobs []Job
+	for _, job := range scheduler.Jobs() {
+		if !strings.HasPrefix(job.Name(), "builtin_") {
+			var schedule, opname string
+			for _, tag := range job.Tags() {
+				if strings.HasPrefix(tag, "SCHEDULE:") {
+					schedule = strings.TrimPrefix(tag, "SCHEDULE:")
+				}
+				if strings.HasPrefix(tag, "OPNAME:") {
+					opname = strings.TrimPrefix(tag, "OPNAME:")
+				}
+			}
+			jobs = append(jobs, Job{Name: job.Name(), Schedule: schedule, OpName: opname})
+		}
+	}
+	tx := db.Clauses(onConflictClause).Create(jobs)
+	if tx.Error != nil {
+		log.WithError(tx.Error).Fatalf("could not persist jobs")
+	}
+	log.Infof("jobs have been persisted")
+	err := scheduler.Shutdown()
+	if err != nil {
+		log.WithError(err).Error("could not gracefully stop scheduler")
+	}
+	os.Exit(0)
+}
+
 func StartSchedules() error {
 	// TODO: Timezone in config, preferably UNIX style (Europe/Berlin)
 	log.Infof("Starting scheduler")
@@ -60,38 +114,38 @@ func StartSchedules() error {
 		log.Errorf("cannot open db: %v", err)
 		return err
 	}
+	err = db.AutoMigrate(&Job{})
+	if err != nil {
+		return err
+	}
 	var jobs []Job
 	db.Find(&jobs)
-	for _, job := range jobs {
-		_, err = scheduler.NewJob(gocron.CronJob(job.Schedule, true), gocron.NewTask(func() { bootstrapJob(job.OpName) }))
-		if err != nil {
-			log.Errorf("cannot recreate job: %v", err)
-			return err
+	// recreate jobs from DB and assert default jobs not present in DB
+	// mint the append parameter order
+	for _, job := range append(jobs, defaultJobs...) {
+		if !doesJobWithNameExist(job.Name) {
+			_, err = scheduler.NewJob(
+				gocron.CronJob(job.Schedule, true),
+				gocron.NewTask(func() { bootstrapJob(job.OpName) }),
+				gocron.WithName(job.Name),
+				gocron.WithTags("SCHEDULE:"+job.Schedule, "OPNAME:"+job.OpName),
+			)
+			if err != nil {
+				log.WithError(err).Errorf("cannot schedule job `%s`", job.Name)
+				return err
+			}
+			log.Debugf("scheduled job `%s`", job.Name)
 		}
-		log.Infof("job %+v", job)
 	}
-	// check if sunset job exists, create if not
-	if !doesJobWithNameExist("builtin_sunset_gen") {
-		_, err = scheduler.NewJob(gocron.CronJob("00 00 15 * * *", true), gocron.NewTask(func() { bootstrapJob("sunset") }), gocron.WithName("builtin_sunset_gen"))
-		if err != nil {
-			log.Errorf("cannot schedule sunset job: %v", err)
-			return err
-		}
-		log.Infof("scheduled sunset job")
-	}
+	log.Infof("jobs loaded")
 
-	if !doesJobWithNameExist("userdef_wakeup-alarm") {
-		_, err = scheduler.NewJob(gocron.CronJob("00 20 07 * * *", true), gocron.NewTask(func() { bootstrapJob("wakeup") }), gocron.WithName("userdef_wakeup-alarm"))
-		if err != nil {
-			log.Errorf("cannot schedule wakeup job: %v", err)
-			return err
-		}
-		log.Infof("scheduled wakeup job")
-	}
 	scheduler.Start()
-	// TODO: persist created jobs to db
 	//go myJobs.PingMasterBulbRoutine()
-	log.Infof("Scheduler started")
+	log.Infof("scheduler started")
 
+	signals = make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+
+	go interruptHandler()
 	return nil
 }
