665fb591972f1a8e38ba1056adfaadba55fda714
nipsufn <nipsufn+gh@pm.me>
2025-07-08 21:37:10 +0200

    feat: postpone alarm until date

--- cmd/serve/alarm.go
+++ cmd/serve/alarm.go
@@ -41,8 +41,47 @@ func handleAlarmApiRequest(w http.ResponseWriter, r *http.Request) {
 			}
 			w.WriteHeader(http.StatusOK)
 			return
+		case "until":
+			log.Tracef("Processing API call: delete alarms until")
+			tmpNow := time.Now()
+			until := r.URL.Query().Get("until")
+			parsedUntil, err := time.Parse("2006-01-02", until)
+			if err != nil {
+				log.Errorf("Processing API call: cannot parse until date")
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			updatedUntil := time.Date(
+				parsedUntil.Year(),
+				parsedUntil.Month(),
+				parsedUntil.Day(),
+				tmpNow.Hour(),
+				tmpNow.Minute(),
+				tmpNow.Second(),
+				tmpNow.Nanosecond(),
+				tmpNow.Location(),
+			)
+			scheduler.RemoveByTags("OPNAME:wakeup")
+			jobName := "builtin_postponewakeup"
+			scheduler.RemoveByTags(jobName)
+			_, err = scheduler.NewJob(
+				gocron.OneTimeJob(
+					gocron.OneTimeJobStartDateTime(updatedUntil)),
+				gocron.NewTask(func() {
+					// reload all jobs from db including wakeup
+					schedule.RestartSchedules(scheduler)
+				}),
+				gocron.WithName(jobName),
+				gocron.WithTags(jobName))
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(err.Error()))
+				return
+			}
+			w.WriteHeader(http.StatusOK)
+			return
 		default:
-			log.Errorf("Processing API call")
+			log.Errorf("Processing API call: unknown operation")
 			w.WriteHeader(http.StatusBadRequest)
 		}
 
